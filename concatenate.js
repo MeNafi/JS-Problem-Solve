ff0c822b0b1b97cc04032d48df1c9c5892689644
MeNafi <nayeefsarkernafi@gmail.com>
2026-02-05 19:07:50 +0600

    track the vaule of variable (solve problem 3)

--- concatenate.js
+++ concatenate.js
@@ -2,3 +2,9 @@ let first = "Nafi";
 let last = "Sarker";
 
 console.log(`${first} ${last}`);
+
+
+let a = 24;
+console.log(a);
+a = 32;
+console.log(a);
---
+++ track.js
@@ -0,0 +1,4 @@
+let hello = "it's old one";
+console.log(hello);
+hello ="it was updated";
+console.log(hello);
