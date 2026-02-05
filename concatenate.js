bc368558036d1c7b4452a7d705254d1b590f4155
MeNafi <nayeefsarkernafi@gmail.com>
2026-02-05 19:21:44 +0600

    track the vaule of variable (solve problem 2)

--- concatenate.js
+++ concatenate.js
@@ -1,10 +1,6 @@
 let first = "Nafi";
 let last = "Sarker";
-
+// using template literal
 console.log(`${first} ${last}`);
 
 
-let a = 24;
-console.log(a);
-a = 32;
-console.log(a);
--- find type.js
+++ find type.js
@@ -1,6 +1,6 @@
 let value = "true";
 console.log(typeof value);
 
-// if am i used number it show the number as a output
+// if am i used number like 23 it show the number as a output
 let num = 23;
 console.log(typeof num);
