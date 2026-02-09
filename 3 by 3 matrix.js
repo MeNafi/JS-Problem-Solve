c6007a7503be29a98092d941a73cd2b90bb9e3b7
MeNafi <nayeefsarkernafi@gmail.com>
2026-02-09 20:18:36 +0600

    solve problem

---
+++ 3 by 3 matrix.js
@@ -0,0 +1,11 @@
+let hold = 1;
+for (let i = 1; i <= 3; i++) {
+    let row = "";
+
+    for (let j = 1; j <= 3; j++) {
+        row += hold + "";
+        hold ++;
+    }
+
+    console.log(row);
+}
