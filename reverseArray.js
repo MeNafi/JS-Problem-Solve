2a382c32419b071e2870b7943b99542288b94610
MeNafi <nayeefsarkernafi@gmail.com>
2026-02-11 20:18:50 +0600

    solve problem

--- reverseArray.js
+++ reverseArray.js
@@ -1,3 +1,15 @@
+// actual pattern to find reverse array
+let arr = [1,2,3,4,5];
+for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+    let temp = arr[i];
+    arr[i] = arr[arr.length -1 - i];
+    arr[arr.length -1 - i] = temp;
+
+}
+
+console.log(arr);
+
+
 // write like this reverse array
 let reverseArray = [1,2,3,4,5];
 
