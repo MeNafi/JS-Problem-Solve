c5d38cb36c3d9c4c32d838b76632e6cbcadaa5f1
MeNafi <nayeefsarkernafi@gmail.com>
2026-02-12 00:21:41 +0600

    solve problem

--- find array 2nd element.js
+++ find array 2nd element.js
@@ -1,3 +1,18 @@
 let arr = [1,2,3,,4,5];
 
 console.log(arr[1]);
+
+
+// using function
+
+function getSecondElement(findArray) {
+    return findArray [1];
+}
+
+
+const arr = [1, 2, 3, 4, 5];
+
+const secondNumber = getSecondElement(arr);
+console.log(secondNumber);
+
+
