a7a750dade05ef4ad2dc606773c960e4174dc878
MeNafi <nayeefsarkernafi@gmail.com>
2026-02-22 23:57:59 +0600

    most optimized bubble sort array problem

--- sort ascending order.js
+++ sort ascending order.js
@@ -1,8 +1,8 @@
 let arr = [18, 13 ,64, 23 ,45];
 
-for (let j = 0; j < arr.length; j++){
+for (let j = 0; j < arr.length -1; j++){
 
-  for (let i = 0; i < arr.length - 1; i++ ){
+  for (let i = 0; i < arr.length - j - 1; i++ ){
    if (arr[i] > arr [i+1]){
     let temp = arr[i];
     arr[i] = arr [i+1];
