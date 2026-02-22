b2f3edb61ab03458c1aa163c1815954c8ccaa0de
MeNafi <nayeefsarkernafi@gmail.com>
2026-02-22 23:40:27 +0600

    solve problem

---
+++ include.js
@@ -0,0 +1,4 @@
+let arr = [1,2,3,4,5,6];
+
+// here check the element is present in the array or not if it is present then it will return true otherwise it will return false.
+console.log(arr.includes(3));
