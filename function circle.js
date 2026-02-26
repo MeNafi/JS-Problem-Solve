181025febc265d483c30ebfb107eaa4d1f1aad73
MeNafi <nayeefsarkernafi@gmail.com>
2026-02-26 22:39:16 +0600

    solve problem

--- function circle.js
+++ function circle.js
@@ -4,3 +4,14 @@ function area (r) {
 
 console.log(area(5));
 
+
+// if am i used arrow function to solve this problem
+const area = (r) => {
+    const pi = Math.PI;
+    const radius = r * r;
+    const result = pi * radius;
+
+    return Math.round(result);
+};
+
+console.log (area(5));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+function area (r) {
+//     return Math.round (Math.PI * r * r);
+// }
+
+// console.log(area(5));
