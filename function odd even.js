2b11fc15b4f9e54690497bbeab1dfc620c3c6449
MeNafi <nayeefsarkernafi@gmail.com>
2026-02-26 21:53:18 +0600

    solve problem

--- function odd even.js
+++ function odd even.js
@@ -9,3 +9,18 @@ function evod (value) {
 console.log(evod (11));
 console.log(evod(12));
 
+
+// if am i use arrow function to solve this problem
+
+const evod = (value) => {
+  if (value % 2 === 0) {
+    return "even";
+  }
+  else {
+    return "odd";
+  }
+ 
+};
+
+console.log (evod(11));
+console.log (evod(12));
