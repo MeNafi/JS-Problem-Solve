0004e417f176fd1464ad83c642ac67dab71b6a70
MeNafi <nayeefsarkernafi@gmail.com>
2026-02-27 23:11:27 +0600

    solve problem

--- function find maximum.js
+++ function find maximum.js
@@ -12,3 +12,20 @@ function max (a,b) {
 
 }
 console.log(max(11, 24));
+
+
+// if am i used arrow function to solve this problem
+
+const max = (a,b) => {
+    if (a > b){
+        return a;
+    }
+    else if (a < b) {
+        return b;
+    }
+    else {
+        return "Both are equal";
+    }
+}
+
+console.log(max(11,24));
