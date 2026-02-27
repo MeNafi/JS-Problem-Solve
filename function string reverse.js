8bff750c76a613655a956bab99b415b79c3adb29
MeNafi <nayeefsarkernafi@gmail.com>
2026-02-27 23:26:59 +0600

    problem solve

--- function string reverse.js
+++ function string reverse.js
@@ -2,3 +2,11 @@ function abcd(value) {
     return value.split("").reverse().join("");
 }
 console.log(abcd("Nafiiii"));
+
+// if am i used arrow function to solve this problem
+
+const reverseString = (value) => {
+    return value.split("").reverse().join("");
+}
+
+console.log(reverseString("Nafiiii"));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+function abcd(value) {
+//     return value.split("").reverse().join("");
+// }
+// console.log(abcd("Nafiiii"));
