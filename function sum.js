5db677512aa30127369559e87593c6476c7429d0
MeNafi <nayeefsarkernafi@gmail.com>
2026-03-13 21:50:20 +0600

    solve problem

--- function sum.js
+++ function sum.js
@@ -5,3 +5,14 @@ function sum(a, b) {
 }
 
 sum(20, 20);
+
+//if am i used arrow function to solve this problem
+
+const sum=(a, b) => {
+    const sum = a+b;
+    console.log("Sum is", sum);
+    
+    }
+
+sum(400, 600);
+return sum;
