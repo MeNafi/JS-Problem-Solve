8d3e632ab1efba3fae2cdbeea122d64389532cff
MeNafi <nayeefsarkernafi@gmail.com>
2026-03-13 22:09:31 +0600

    update the problem solution

--- function rectangle.js
+++ function rectangle.js
@@ -1,4 +1,12 @@
 function rec(len, wid) {
+  if (len <= 0) {
+    throw new RangeError("Length must be a positive number");
+  }
+
+  if (wid <= 0) {
+    throw new RangeError("Width should be a positive number");
+  }
+
   const area = len * wid;
   console.log("Area od rectangle", area);
 
@@ -9,10 +17,10 @@ rec(30, 30);
 
 // if am i used arrow function to solve this problem
 
-const rec = (len, wid) => {
-  const area = len * wid;
-  console.log("Area of rectangle", area);
-};
+// const rec = (len, wid) => {
+//   const area = len * wid;
+//   console.log("Area of rectangle", area);
+// };
 
-rec(40, 40);
-return rec;
+// rec(40, 40);
+// return rec;
