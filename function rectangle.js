c6b57ceae5fb6e7bb2c74c152a87bb8e7a41fe8a
MeNafi <nayeefsarkernafi@gmail.com>
2026-03-13 22:00:49 +0600

    solve problem

--- function rectangle.js
+++ function rectangle.js
@@ -1,8 +1,18 @@
-function rec(len, wid){
-    const area = len * wid;
-    console.log("Area od rectangle", area);
-    
-    return area;
+function rec(len, wid) {
+  const area = len * wid;
+  console.log("Area od rectangle", area);
+
+  return area;
 }
 
 rec(30, 30);
+
+// if am i used arrow function to solve this problem
+
+const rec = (len, wid) => {
+  const area = len * wid;
+  console.log("Area of rectangle", area);
+};
+
+rec(40, 40);
+return rec;
